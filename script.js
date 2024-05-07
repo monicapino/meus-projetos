072975b93de4e80f9d49fc88c5c7ba70648b32c5
Monica Pino <112265101+monicapino@users.noreply.github.com>
2024-05-07 14:17:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 function toggleMode() {
   const html = document.documentElement
-
-  //if(html.classList.contains('light')) {
-  //  html.classList.remove('light')
-  // } else {
-  //  html.classList.add('light')
-  // }
   html.classList.toggle("light")
-
-  // pegar a tag img
   const img = document.querySelector("#profile img")
-  // substituir a imagem
   if (html.classList.contains("light")) {
-    //se tiver light mode, adiconar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    // se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/Avatar.png")
-  } 
-  
+  }
 }
